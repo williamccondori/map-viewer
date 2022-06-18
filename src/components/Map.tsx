e7d6614c799fb15b39a9dc4473e10ac6823a3876
William Condori Quispe <williamccondori@outlook.com>
2022-06-18 01:24:43 -0500

    add wms example

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,33 +1,110 @@
-import React from 'react';
+import React, { MouseEventHandler, useState } from 'react';
 import {
+  LayersControl,
   MapContainer,
   TileLayer,
-  Marker,
-  Popup,
+  WMSTileLayer,
 } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import { Button } from '@chakra-ui/react';
 
 interface MapProps {
   className?: string;
 }
 
 export default function Map({ className }:MapProps): JSX.Element {
+  const [baseMaps] = useState([{
+    key: 1,
+    name: 'MapBox',
+    isDefault: true,
+    tileSize: 512,
+    zoomOffset: -1,
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, '
+    + 'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    url: 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
+  }, {
+    key: 1,
+    name: 'MapBox Satellite',
+    isDefault: false,
+    tileSize: 512,
+    zoomOffset: -1,
+    maxZoom: 18,
+    id: 'mapbox/satellite-streets-v11',
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, '
+    + 'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    url: 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
+  }]);
+
+  interface IWmsLayer{
+    key: number;
+    url: string;
+    layers: string;
+    format: string;
+    isTransparent: boolean;
+    attribution: string;
+    version: string;
+  }
+
+  const defaultWmsLayers: IWmsLayer = {
+    key: 1,
+    url: 'http://ide.regionmadrededios.gob.pe:9001/geoserver/goremadide/wms',
+    layers: 'goremadide:distritos,goremadide:prueba_shapefile_2022',
+    format: 'image/png',
+    isTransparent: true,
+    attribution: '&copy; <a href="http://ide.regionmadrededios.gob.pe/portal/" target="_blank">Gobierno Regional de Madre de Dios</a>',
+    version: '1.1.1',
+  };
+
+  const [wmsLayers, setWmsLayers] = useState([defaultWmsLayers]);
+
+  const handleClick = (actualServices: IWmsLayer[]): any => {
+    const actualService:IWmsLayer = actualServices[0];
+    actualService.layers = 'goremadide:distritos';
+    setWmsLayers([actualService]);
+  };
+
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} className={className}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup.
-          {' '}
-          <br />
-          {' '}
-          Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <>
+      {/* <Button colorScheme="blue" onClick={() => handleClick(wmsLayers)}>Click me!</Button> */}
+      <MapContainer center={[-10.1884015, -75.6500792]} zoom={6} className={className}>
+        <LayersControl position="topright">
+          {
+        baseMaps.map((baseMap) => (
+          <LayersControl.BaseLayer
+            checked={baseMap.isDefault}
+            key={baseMap.key}
+            name={baseMap.name}
+          >
+            <TileLayer
+              id={baseMap.id}
+              attribution={baseMap.attribution}
+              url={baseMap.url}
+              tileSize={baseMap.tileSize}
+              zoomOffset={baseMap.zoomOffset}
+              maxZoom={baseMap.maxZoom}
+              zIndex={0}
+            />
+          </LayersControl.BaseLayer>
+        ))
+      }
+        </LayersControl>
+        {
+        wmsLayers.map((wmsLayer) => (
+          <WMSTileLayer
+            key={wmsLayer.key}
+            url={wmsLayer.url}
+            layers={wmsLayer.layers}
+            transparent={wmsLayer.isTransparent}
+            format={wmsLayer.format}
+            attribution={wmsLayer.attribution}
+            zIndex={999}
+          />
+        ))
+      }
+      </MapContainer>
+    </>
   );
 }
 
